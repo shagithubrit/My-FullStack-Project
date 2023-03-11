dcde40cff2c65dc67ef1e929fefb2ae607246033
Chandan Kumar <chandan181singh@gmail.com>
2023-03-11 14:05:40 +0530

    check

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const sessionSecrete = "mysitesessionsecrete";
+
+const emailUser = "chandan181singh@gmail.com";
+const emailPassword = "tkvsezmhdpiurmem";
+
+const clientId = "470744417789-5ossefd6bs1qfceq0mpnk8do9enuanv7.apps.googleusercontent.com";
+const clientSecret = "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";
+
+module.exports ={
+    sessionSecrete,
+    emailUser,
+    emailPassword,
+    clientId,
+    clientSecret
+}
